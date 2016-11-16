4a600e4d41f38ed8221589dd889e4ab6e9dc732b
adrian cordova y quiroz <adriancordovayquiroz@adrians-mbp-3.dynamic.ucsd.edu>
2016-11-16 14:53:05 -0800

    extractBit.s complete

--- extractBit.s
+++ extractBit.s
@@ -13,13 +13,28 @@ extractBit:
     push {r4-r11, ip, lr}
 
     @ YOUR CODE GOES HERE
-    @-----------------------
-    
+    MOV R8, #0         @ value for 0
+    MOV R6, #1         @ value for 1
+    LSL R6, R6, R1     @1<<index
+    AND R7, R0, R6     @c & mask stored in r7
+    CMP R7, R8         @compare extractBit to 0
+    BGT greaterthan    @branch to greater than
+    B lessthan         @else, branch to less than
+     
+greaterthan: 
+         MOV R0, #1    @return 1 because greaterthan
+         B return
+lessthan:              @return 0 becasuse lessthan
+         MOV R0, #0
+
+return:                @make sure r0 is return value 
+         MOV R0, R0
     @ (your code)
     @ put your return value in r0 here:
 
+    @return value
+
     @-----------------------
-return:
     @ restore caller's registers
     pop {r4-r11, ip, lr}
 
