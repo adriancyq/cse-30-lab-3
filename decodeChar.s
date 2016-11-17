1517020f57142189ce063c5b9c93330e0c921a01
Jonathan Chiu <jonathanchiu@jonathans-mbp.attlocal.net>
2016-11-17 12:48:37 -0800

    started decodeChar

--- decodeChar.s
+++ decodeChar.s
@@ -14,9 +14,49 @@ decodeChar:
 
     @ YOUR CODE GOES HERE
     @-----------------------
+
+    @ Initialize indices for char array and MAPPING array
+    MOV R1, #0
+    MOV R2, #0
+
+    @ Initialize exponent used for determining index of MAPPING array
+    MOV R3, #5
+
+loop:
     
+    @ For (index = 0; index < 6; index++)
+    CMP R1, #6
+    BGE foundMappingIndex
+
+    @ Load char at specified position from the array
+    LDR R4, [R0, R1, LSL #2]
+
+    @ Subtract the ASCII value for '0' to convert to int 
+    SUB R4, R4, #48
+
+    @ Multiply by exponent to convert binary to decimal
+    LSL R4, R4, R3
+
+    @ Add to index of MAPPING array 
+    ADD R2, R2, R4 
+
+    @ Update counters
+    ADD R1, R1, #1
+    SUB R3, R3, #1
+    B loop
+
+foundMappingIndex:
+    
+    @ Load address of MAPPING array
+    LDR R5, =MAPPING 
+
+    @ Get char at generated index (R2)
+    LDRB R6, [R5, R2, LSL #2]
+
+
     @ (your code)
     @ put your return value in r0 here:
+    MOV R0, R6 
 
     @-----------------------
 return:
